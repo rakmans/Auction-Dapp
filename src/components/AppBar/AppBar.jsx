df183d20f0ef767d9e3d27419e68d7e90dd084b1
rakmans <rakmans.team@gmail.com>
2024-04-22 19:16:58 +0330

    edit appbar and footer

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,12 +1,12 @@
 import {
-  CssBaseline,
-  Fab,
-  Container,
-  AppBar,
-  Box,
-  Toolbar,
-  useTheme,
-  Typography,
+    CssBaseline,
+    Fab,
+    Container,
+    AppBar,
+    Box,
+    Toolbar,
+    useTheme,
+    Typography,
 } from "@mui/material";
 import { UilAngleUp } from "@iconscout/react-unicons";
 import { NavLink } from "react-router-dom";
@@ -16,61 +16,72 @@ import { Mode, rakmans } from "./index";
 import WalletConnect from "../../components/WalletConnectButton";
 
 const Appbar = () => {
-  const theme = useTheme();
+    const theme = useTheme();
 
-  return (
-    <>
-      <CssBaseline />
-      <ElevationScroll>
-        <AppBar
-          className="appBar"
-          sx={{
-            width: "100%",
-            p: 0.5,
-            backdropFilter: "blur(3px)",
-            borderBottom:
-              theme.palette.mode === "dark" ? "3px solid #40679E" : "",
-            WebkitBackdropFilter: "blur(3px)",
-            zIndex: 9,
-          }}
-        >
-          <Container maxWidth="xl">
-            <Toolbar disableGutters>
-              <Box
-                sx={{
-                  mr: 2,
-                  display: "flex",
-                  flexGrow: 1,
-                  fontSize: "30px",
-                }}
-              >
-                <Box component="img" src={rakmans} alt="logo" width={50} />
-                <Typography
-                  variant="h4"
-                  mt={0.9}
-                  ml={1}
-                  display={{ md: "block", xs: "none" }}
-                >
-                  Rakmans
-                </Typography>
-              </Box>
+    return (
+        <>
+            <CssBaseline />
+            <ElevationScroll>
+                <AppBar
+                    className='appBar'
+                    sx={{
+                        width: "100%",
+                        p: 0.5,
+                        backdropFilter: "blur(3px)",
+                        borderBottom:
+                            theme.palette.mode === "dark"
+                                ? "3px solid #40679E"
+                                : "",
+                        WebkitBackdropFilter: "blur(3px)",
+                        zIndex: 9,
+                    }}>
+                    <Container maxWidth='xl'>
+                        <Toolbar disableGutters>
+                            <Box
+                                component='a'
+                                target='_blank'
+                                href='https://rakmans.github.io/'
+                                sx={{
+                                    mr: 2,
+                                    display: "flex",
+                                    flexGrow: 1,
+                                    fontSize: "30px",
+                                    color:"white",
+                                    textDecoration:"none"
+                                }}>
+                                <Box
+                                    component='img'
+                                    src={rakmans}
+                                    alt='logo'
+                                    width={50}
+                                />
+                                <Typography
+                                    variant='h4'
+                                    mt={0.9}
+                                    ml={1}
+                                    display={{ md: "block", xs: "none" }}>
+                                    Rakmans
+                                </Typography>
+                            </Box>
 
-              <Box sx={{ flexGrow: { xs: 1, sm: 0 } }} display={"flex"}>
-                <WalletConnect />
-              </Box>
-              <Mode />
-            </Toolbar>
-          </Container>
-        </AppBar>
-      </ElevationScroll>
-      <Box id="back-to-top-anchor" />
-      <ScrollTop>
-        <Fab color="primary" size="small">
-          <UilAngleUp />
-        </Fab>
-      </ScrollTop>
-    </>
-  );
+                            <Box
+                                sx={{ flexGrow: { xs: 1, sm: 0 } }}
+                                display={"flex"}>
+                                <WalletConnect />
+                            </Box>
+                            <Mode />
+                        </Toolbar>
+                    </Container>
+                </AppBar>
+            </ElevationScroll>
+            <Box id='back-to-top-anchor' />
+            <ScrollTop>
+                <Fab color='primary' size='small'>
+                    <UilAngleUp />
+                </Fab>
+            </ScrollTop>
+        </>
+    );
 };
 
 export default Appbar;
